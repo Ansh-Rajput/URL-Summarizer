2dbeffa7543ff30c1151540bc3579466d6e5436b
Ansh-Rajput <anshravikrish425317@gmail.com>
2023-09-02 23:42:08 +0530

    project compleated all functionality are working properly.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex w-full mb-10 pt-3 justify-between items-center">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button type="button" onClick={() => window.open("urlofGithubRepo")} className='black_btn'>
+          {/* Todo */}
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simpify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
